bb85539826ea4862f685707e50bfbc5b8fb1c5f7
Huzaib <huzaibulhassan@gmail.com>
2020-07-06 21:08:19 +0530

    Updated to Binary search to reduce the time complexity to O(log(n))

--- LinearSearch.java
+++ LinearSearch.java
@@ -2,15 +2,15 @@ package FirstProgram;
 
 public class LinearSearch {
 	
-	public static int Search(int arr[], int u) {int k=0;
-		for(int i=0; i<arr.length; i++) {
-			if(arr[i]==u)k=i;
-		}return k;
+	public static int Search(int arr[], int u, int low, int high) {int mid = low + (high-low)/2;
+	if(arr[mid]==u)return mid;
+	else if(u>arr[mid]) return Search(arr, u, mid+1, high);
+	else return Search(arr, u, low, mid-1);
 	}
 	public static void main(String args[]) {
 		int a[] = {1,2,3,4,5,6,7,8,9,0};
 		int k=8;
-		System.out.println(Search(a,k));
+		System.out.println(Search(a,k, 0, 9));
 		
 	}
 
