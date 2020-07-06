d1afc873eea4df508f272e714496eb817d9ce689
Huzaib <huzaibulhassan@gmail.com>
2020-07-06 21:09:59 +0530

    Changed the number sequence inputted

--- LinearSearch.java
+++ LinearSearch.java
@@ -8,7 +8,7 @@ public class LinearSearch {
 	else return Search(arr, u, low, mid-1);
 	}
 	public static void main(String args[]) {
-		int a[] = {1,2,3,4,5,6,7,8,9,0};
+		int a[] = {0,1,2,3,4,5,6,7,8,9};
 		int k=8;
 		System.out.println(Search(a,k, 0, 9));
 		
